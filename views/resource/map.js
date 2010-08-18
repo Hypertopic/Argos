6cb302e1484d3d4ba275f28e3dfc278e98847ac6
Aurelien Benel <aurelien.benel@utt.fr>
2010-08-18 11:56:59 +0200

    FEATURE: Coherence improved on views format.

--- views/resource/map.js
+++ views/resource/map.js
@@ -1,5 +1,5 @@
 function(o) {
   if (o.item_corpus) {
-          emit([o.resource], {corpus:o.item_corpus, item:o._id});
+          emit([o.resource], {item:{corpus:o.item_corpus, id:o._id}});
   }
 }
