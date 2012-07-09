d411aace9e85cd2e1181a19722d8cecc99f18d54
Aurelien Benel <aurelien.benel@utt.fr>
2012-07-09 09:28:31 +0200

    API: update_seq as a database metadata.

--- shows/index.js
+++ shows/index.js
@@ -1,18 +1,19 @@
 function(doc, req) {
-  var revision = '0.8'; //Current version
   if('Accept' in req.headers && req.headers['Accept'].indexOf('json') > 0) {
     return {
-        body : JSON.stringify({'service':'Argos', 'revision': revision}),
-        headers : { 
-          "Content-Type" : "application/json",
-        }
+      body : JSON.stringify({
+        service: 'Argos', 
+        revision: '3.12.07.09',
+        update_seq: req.info.update_seq
+      }), headers : { 
+        'Content-Type': 'application/json',
+      }
     }
-  }
-  else {
+  } else {
     return {
-        body : '<html><body><h3>Welcome to Argos!</h3><p>version ' + revision + '.</p></body></html>',
+        body : '<html><body><h3>Welcome to Argos!</h3></body></html>',
         headers : { 
-          "Content-Type" : "text/html",
+          'Content-Type' : 'text/html',
         }
     }
   }
