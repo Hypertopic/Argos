9170fa4013342572d106998057d2ff27e659fe8f
Aurelien Benel <aurelien.benel@utt.fr>
2018-09-26 13:59:56 +0200

    REFACTORING: No need for Hypertopic protocol v1 emulation.
    Broken compatibility with Porphyry v6 transitional (and older).

--- views/corpusV1/map.js
+++
@@ -1,7 +0,0 @@
-function(o) {
-  if (o.item_corpus) {
-    emit([o.item_corpus], {item:{id:o._id, name:o.item_name}});
-  } else if (o.corpus_name) {
-    emit([o._id], {name:o.corpus_name});
-  }
-}
