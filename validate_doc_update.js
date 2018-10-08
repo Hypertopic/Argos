44e32c8896d2cf7fc84f15dce81159ef09f6c222
Aurelien Benel <aurelien.benel@utt.fr>
2018-10-08 21:15:37 +0200

    FEATURE: Edition requires authentication.

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,5 +1,10 @@
 function (newDoc, oldDoc, userCtx, secObj) {
   var v = require("lib/validate").init(newDoc, oldDoc, userCtx, secObj);
+
+  if (!userCtx.name) {
+    v.unauthorized("Please log in first.");
+  }
+
   //Validation for viewpoint, topics in viewpoint should be a DAG
   if(newDoc.viewpoint_name){
     if(!newDoc.topics) return true;
