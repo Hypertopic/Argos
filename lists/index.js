Hypertopic/Argos

commit 9c830e67dcadd851795689a63208f8eee70787ee

FEATURE: Get usage statistics (closes #40).

lists/index.js
@@ -1,0 +1,21 @@
+function(head, req) {
+  provides('json', function() {
+    return {
+      body: JSON.stringify({
+        service: 'Argos',
+        revision: '3.16.03.16',
+        update_seq: req.info.update_seq
+      })
+    }
+  });
+  provides('html', function() {
+    send('<html><body><h1>Argos</h1><table>');
+    while (r = getRow()) {
+      send('<tr>');
+      send('<th>' + r.key + '</th>');
+      send('<td>' + r.value + '</td>');
+      send('</tr>');
+    }
+    send('</table></body></html>');
+  });
+}

shows/index.js
@@ -1,16 +1,0 @@
-function(doc, req) {
-  provides('json', function() {
-    return {
-      body: JSON.stringify({
-        service: 'Argos', 
-        revision: '3.16.03.16',
-        update_seq: req.info.update_seq
-      })
-    }
-  });
-  provides('html', function() {
-    return {
-      body: '<html><body><h3>Welcome to Argos!</h3></body></html>',
-    }
-  });
-}

views/stats/map.js
@@ -1,0 +1,21 @@
+function (o) {
+  if (o.viewpoint_name) {
+    emit('viewpoints', 1);
+    if (o.topics) {
+      emit('viewpoints-with-topics', 1);
+      emit('topics', Object.keys(o.topics).length);
+    };
+  } else if (o.corpus_name) {
+    emit('corpora', 1);
+  } else if (o.item_name) {
+    emit('items', 1);
+    if (o.topics) {
+      emit('items-with-topics', 1);
+      emit('topics-on-items', Object.keys(o.topics).length);
+    };
+    if (o.highlights) {
+      emit('items-with-highlights', 1);
+      emit('topics-on-fragments', Object.keys(o.highlights).length);
+    }
+  }
+}

views/stats/reduce.js
@@ -1,0 +1,1 @@
+_sum

commit dbe087501f90f2b36e831ea77c23f7adf115c4fb

FIX: Error 500 on JSON flavor of the API endpoint.
Caused by 9c830e67dcadd851795689a63208f8eee70787ee.

lists/index.js
@@ -1,12 +1,12 @@
 function(head, req) {
   provides('json', function() {
-    return {
-      body: JSON.stringify({
+    send(
+      JSON.stringify({
         service: 'Argos',
         revision: '3.16.03.16',
         update_seq: req.info.update_seq
       })
-    }
+    );
   });
   provides('html', function() {
     send('<html><body><h1>Argos</h1><table>');