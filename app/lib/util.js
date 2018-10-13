0b6091cf452925d8c173e76507ac0b2952b34c93
Aurelien Benel <aurelien.benel@utt.fr>
2018-10-13 16:14:13 +0200

    DOC: Explain to coders how XML encoding is computed.

--- app/lib/util.js
+++ app/lib/util.js
@@ -2,6 +2,7 @@ exports.xmlencode = function(data) {
   switch (typeof data) {
     case "object":
       data = data[0];
+      // No break so that the first element of the list is processed as a string.
     case "string":
       data = data.replace(/\&/g,'&'+'amp;')
         .replace(/</g,'&'+'lt;')
