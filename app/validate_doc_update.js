2673b283fa40a900ea213493721850b451d625ad
Aurelien Benel <aurelien.benel@utt.fr>
2019-03-27 17:09:21 +0100

    REFACTORING: Number of lines of code reduced.

--- app/validate_doc_update.js
+++ app/validate_doc_update.js
@@ -1,16 +1,16 @@
 function (newDoc, oldDoc, userCtx, secObj) {
-  var v = require("lib/validate").init(newDoc, oldDoc, userCtx, secObj);
-
   if (!userCtx.name) {
-    v.unauthorized("Please log in first.");
+    throw({unauthorized: 'Please log in first.'});
   }
 
   //Validation for viewpoint, topics in viewpoint should be a DAG
   if(newDoc.viewpoint_name){
     if(!newDoc.topics) return true;
     var viewpoint = require("lib/viewpoint");
-    var sorted = viewpoint.topologicalSort(newDoc.topics);
-    v.assert(sorted, "There is a cycle in the viewpoint between topics!");
-    return true;
+    if (!viewpoint.topologicalSort(newDoc.topics)) {
+      throw({
+        forbidden: 'There is a cycle in the viewpoint between topics!'
+      });
+    }
   }
 }
