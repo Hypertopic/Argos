5bc6369ab48ac680d6290a85df395e77573a4967
Axel Garnier <axel.garnier@utt.fr>
2020-05-29 17:50:40 +0200

    REFACTORING: Reduce viewpoint payload on Argos (#56)

--- app/views/viewpoint/map.js
+++ app/views/viewpoint/map.js
@@ -1,24 +1,36 @@
 function(o) {
   if (o.viewpoint_name) {
+    var entry = {};
     //viewpoint name
-    emit([o._id], {name:o.viewpoint_name});
+    entry["name"] = o.viewpoint_name;
     //viewpoint users
-    for each (var u in o.users) {
-      emit([o._id], {user:u});
-    }
+    entry["user"] = o.users;
     //topics
     const topics = o.topics;
+    entry["upper"] = [];
     for (var t in topics) {
+      var entry_t = {};
+      var broaderArray = [];
       //topic name
-      emit([o._id, t], {name:topics[t].name});
+      entry_t["name"] = topics[t].name;
       //topic links
       var broader = topics[t].broader;
       if (broader==null || broader.length==0) {
-        emit([o._id], {upper:{id:t, name:topics[t].name}});
+        entry["upper"].push({
+          id:t,
+          name:topics[t].name
+        });
       } else for each(b in broader) {
-        emit([o._id, t], {broader: {id:b, name:topics[b].name}});
+        broaderArray.push({
+          id:b,
+          name:topics[b].name
+        });
         emit([o._id, b], {narrower: {id:t, name:topics[t].name}});
       }
+      if (broaderArray.length > 0)
+        entry_t["broader"] = broaderArray;
+      emit([o._id, t], entry_t)
     }
+    emit([o._id], entry);
   }
 }
