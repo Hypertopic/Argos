8f9153f56eb8ffd377e47664107b3bbd07a596b6
Aurelien Benel <aurelien.benel@utt.fr>
2021-01-30 16:56:15 +0100

    RELEASE: 4.4.2

--- app/lists/index.js
+++ app/lists/index.js
@@ -3,7 +3,7 @@ function(head, req) {
     send(
       JSON.stringify({
         service: 'Argos',
-        revision: '4.4.1',
+        revision: '4.4.2',
         update_seq: req.info.update_seq
       })
     );
